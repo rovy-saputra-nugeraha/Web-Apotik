474674845f447122082dab9b36356d91be349bb0
rovy-saputra-nugeraha <2101020012@student.umrah.ac.id>
2023-11-27 14:14:59 +0700

    Aplikasi Apotik

---
+++ db_toko.sql
@@ -0,0 +1,263 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Nov 2023 pada 08.12
+-- Versi server: 10.4.32-MariaDB-log
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_toko`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `nama_barang` text NOT NULL,
+  `merk` varchar(255) NOT NULL,
+  `harga_beli` varchar(255) NOT NULL,
+  `harga_jual` varchar(255) NOT NULL,
+  `satuan_barang` varchar(255) NOT NULL,
+  `stok` text NOT NULL,
+  `tgl_input` varchar(255) NOT NULL,
+  `tgl_update` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id`, `id_barang`, `id_kategori`, `nama_barang`, `merk`, `harga_beli`, `harga_jual`, `satuan_barang`, `stok`, `tgl_input`, `tgl_update`) VALUES
+(16, 'BR001', 19, 'Dores 5 Mg', '-', '3000', '6000', 'PCS', '22', '27 November 2023, 13:59', '27 November 2023, 14:11'),
+(17, 'BR002', 19, 'Rizodal 5 Mg', '-', '2000', '3000', 'PCS', '63', '27 November 2023, 14:02', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL,
+  `tgl_input` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `tgl_input`) VALUES
+(18, 'Obat Cair', '27 November 2023, 13:58'),
+(19, 'Obat Tablet', '27 November 2023, 13:58'),
+(20, 'Obat Minyak', '27 November 2023, 13:59');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `id_login` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `pass` char(32) NOT NULL,
+  `id_member` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`id_login`, `user`, `pass`, `id_member`) VALUES
+(1, 'admin', '202cb962ac59075b964b07152d234b70', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `member`
+--
+
+CREATE TABLE `member` (
+  `id_member` int(11) NOT NULL,
+  `nm_member` varchar(255) NOT NULL,
+  `alamat_member` text NOT NULL,
+  `telepon` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gambar` text NOT NULL,
+  `NIK` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `member`
+--
+
+INSERT INTO `member` (`id_member`, `nm_member`, `alamat_member`, `telepon`, `email`, `gambar`, `NIK`) VALUES
+(1, 'Rovy Saputra Nugeraha ', 'Tanjungpinang Timur', '081261461477', 'rovysaputra10@gmail.com', 'Rovy Saputra Nugeraha_Blue.png', '2101020012');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nota`
+--
+
+CREATE TABLE `nota` (
+  `id_nota` int(11) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `jumlah` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `tanggal_input` varchar(255) NOT NULL,
+  `periode` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `jumlah` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `tanggal_input` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `toko`
+--
+
+CREATE TABLE `toko` (
+  `id_toko` int(11) NOT NULL,
+  `nama_toko` varchar(255) NOT NULL,
+  `alamat_toko` text NOT NULL,
+  `tlp` varchar(255) NOT NULL,
+  `nama_pemilik` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `toko`
+--
+
+INSERT INTO `toko` (`id_toko`, `nama_toko`, `alamat_toko`, `tlp`, `nama_pemilik`) VALUES
+(1, 'Apotik Bahari Farma', 'Jl.RSAL Tanjungpinang Barat', '0821', 'Pak Beni');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indeks untuk tabel `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indeks untuk tabel `nota`
+--
+ALTER TABLE `nota`
+  ADD PRIMARY KEY (`id_nota`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`);
+
+--
+-- Indeks untuk tabel `toko`
+--
+ALTER TABLE `toko`
+  ADD PRIMARY KEY (`id_toko`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT untuk tabel `login`
+--
+ALTER TABLE `login`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `member`
+--
+ALTER TABLE `member`
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `nota`
+--
+ALTER TABLE `nota`
+  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT untuk tabel `toko`
+--
+ALTER TABLE `toko`
+  MODIFY `id_toko` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
